367a32f41aa35e1e97c4b783b802f36c402bbf11
Marlon Jestädt <marlon2019@outlook.de>
2025-08-17 19:03:10 +0200

    Update & Add support for personal table

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,9 @@
 
 console.log("SchulPortalPlus loaded!"); 
 advanceMeinUnterricht();
+addPersonalPlan();
+
+
 
 async function advanceMeinUnterricht() {
     if (!window.location.href.startsWith("https://start.schulportal.hessen.de/meinunterricht.php")) return;
@@ -42,36 +45,28 @@ async function addExtraInfo(stundenplan) {
 }
 
 async function addExtraTabs(stundenplan, vertretungsplan) {
-    var tabcontent = document.querySelector(".tab-content");
-    var tabs = document.querySelector(".nav-tabs");
-
-    var stundenplanTab = document.createElement("div");
-    stundenplanTab.className = "tab-pane";
-    stundenplanTab.setAttribute("role", "tabpanel");
-    stundenplanTab.id = "stundenplan";
-    stundenplanTab.innerHTML = stundenplan.querySelector(".tab-content #all").innerHTML;
-    tabcontent.appendChild(stundenplanTab);
-    var stundenplanTabLink = document.createElement("li");
-    stundenplanTabLink.setAttribute("role", "presentation");
-    stundenplanTabLink.innerHTML = '<a href="#stundenplan" aria-controls="stundenplan" role="tab" data-toggle="tab" aria-expanded="false"><i class="fa fa-fw fa-clock"></i> Stundenplan</a>';
-    tabs.appendChild(stundenplanTabLink);
-
-    var vertretungsplanTab = document.createElement("div");
-    vertretungsplanTab.className = "tab-pane";
-    vertretungsplanTab.setAttribute("role", "tabpanel");
-    vertretungsplanTab.id = "vertretungsplan";
-    console.log(vertretungsplan)
-    vertretungsplan.querySelector(".panel-info:nth-child(2)").style.display = "block";
-    vertretungsplan.querySelector(".panel-info:nth-child(3)").style.display = "block";
-
-    vertretungsplanTab.innerHTML = vertretungsplan.querySelector("#content").innerHTML;
-    tabcontent.appendChild(vertretungsplanTab);
-    var vertretungsplanTabLink = document.createElement("li");
-    vertretungsplanTabLink.setAttribute("role", "presentation");
-    vertretungsplanTabLink.innerHTML = '<a href="#vertretungsplan" aria-controls="vertretungsplan" role="tab" data-toggle="tab" aria-expanded="false"><i class="fa fa-fw fa-list"></i> Vertretungsplan</a>';
-    tabs.appendChild(vertretungsplanTabLink);
+    addExtraTab("substitutions", "Vertretungsplan", vertretungsplan.querySelector("#content").innerHTML, "fa-list");
+    addExtraTab("timetable", "Stundenplan", stundenplan.querySelector(".tab-content #all").innerHTML, "fa-clock");
+    await implementPersonalPlan(stundenplan.querySelector("#own table"));
+    addExtraTab("personaltimetable", "Persönlicher Plan", stundenplan.querySelector(".tab-content #own").innerHTML, "fa-user");
 }
 
+async function addExtraTab(tabId, tabName, tabContent, icon) {
+    var tabs = document.querySelector(".nav-tabs");
+    var tabcontent = document.querySelector(".tab-content");
+
+    var newTab = document.createElement("div");
+    newTab.className = "tab-pane";
+    newTab.setAttribute("role", "tabpanel");
+    newTab.id = tabId;
+    newTab.innerHTML = tabContent;
+    tabcontent.appendChild(newTab);
+
+    var newTabLink = document.createElement("li");
+    newTabLink.setAttribute("role", "presentation");
+    newTabLink.innerHTML = '<a href="#' + tabId + '" aria-controls="' + tabName + '" role="tab" data-toggle="tab" aria-expanded="false"><i class="fa fa-fw ' + icon + '"></i> ' + tabName + '</a>';
+    tabs.appendChild(newTabLink);
+}
 
 function convertToLongNames(short) {
     // return the long form name of the subject (e.g. "D" -> "Deutsch")
@@ -113,4 +108,81 @@ function convertToLongNames(short) {
         case "WU-MINT-II.1":
             return "Naturwissenschaften 10";
     }
+}
+
+async function addPersonalPlan() {
+    if (!window.location.href.startsWith("https://start.schulportal.hessen.de/stundenplan.php")) return;
+    var personalTable = document.querySelector("#own table");
+    implementPersonalPlan(personalTable);
+}
+
+async function implementPersonalPlan(personalTable) {
+    var spHTML = await fetch("https://start.schulportal.hessen.de/stundenplan.php").then(response => response.text());
+    var parser = new DOMParser(); 
+    var stundenplan = parser.parseFromString(spHTML, "text/html");
+    var spTable = stundenplan.querySelector("#all table");
+    var lgData = await fetchLerngruppen();
+    console.log("Lerngruppen: ", lgData);
+    var tutor = lgData.find(item => item.name.includes("TUT")).teacher;
+    for (let i = 1; i < spTable.rows.length; i++) {
+        const row = spTable.rows[i];
+
+        for (let j = 1; j < row.cells.length; j++) {
+            const cell = row.cells[j];
+            if (cell.innerHTML.trim() === "") continue; // Skip empty cells
+            //console.log("Processing cell: row " + i + ", column" + j);
+
+            var stunden = cell.querySelectorAll("div");
+            if (stunden.length === 0) continue; // Skip cells without divs
+            Array.from(stunden).forEach(stunde => {
+                var teacher = stunde.querySelector("small");
+                var kurs = stunde.querySelector("b");
+        
+                if (teacher != null) {
+                    teacher = teacher.innerText.trim();
+                    if (kurs != null) {
+                        var kursText = kurs.innerText.trim();
+                        var kursId = kurs.textContent.trim();
+                        console.log("Kurs: " + kursText + ", Lehrer: " + teacher);
+                        if (kursId.startsWith("1Q2ma")) {
+                            kursId = "1Q2m" + kursId.substring(5); // Convert "ma" to "m"
+                        }
+
+                        if (lgData.some(item => item.name === kursId && item.teacher === teacher) 
+                            || (kursText.includes("tut") && teacher.includes(tutor))) {
+                            console.log(kursText + " bei " + teacher + " mit spanne " + cell.rowSpan);
+                            kursText = kursText.substring(3); // Remove "1Q2" prefix
+                            if (kursText[0] == kursText[0].toUpperCase()) {
+                                cell.style.backgroundColor = "#741f1f5e";
+                            }
+                        } else {
+                            stunde.remove();
+                            //console.log("Kurs " + kurs + " bei " + teacher + " not found in lerngruppen data.");
+                        }
+                        kurs.textContent = kursText; // Update kurs text
+                    }
+                }
+            });
+        }
+    }
+    personalTable.innerHTML = spTable.querySelector("tbody").innerHTML;
+}
+
+async function fetchLerngruppen() {
+    var lgHTML = await fetch("https://start.schulportal.hessen.de/lerngruppen.php").then(response => response.text());
+    var parser = new DOMParser(); 
+    var lerngruppen = parser.parseFromString(lgHTML, "text/html");
+    var lgTable = lerngruppen.querySelector("#kurse table");
+    var lgData = [];
+    for (let i = 1; i < lgTable.rows.length; i++) {
+        const row = lgTable.rows[i];
+        var lgName = row.cells[1].querySelector("small").innerText.trim().substring(1).split(" - ")[0];
+        var lgTeacher = row.cells[2].innerText.trim().split("(")[1].slice(0, -1);
+        var lg = {
+            name: lgName,
+            teacher: lgTeacher
+        }
+        lgData.push(lg);
+    }
+    return lgData;
 }
